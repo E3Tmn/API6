d9c620bf31354e47c88324f7b0c768183acde96f
E3Tmn <nick1tapinyagin@yandex.ru>
2023-11-22 18:11:22 +0300

    add check_vk_response

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import random
 import shutil
 
 
+def check_vk_response(response):
+    if response.history:
+            raise requests.HTTPError
+
+
 def publish_comic(image_id, owner_id, token, text, group_id):
     payload = {
         'owner_id': f'-{group_id}',
@@ -18,6 +23,7 @@ def publish_comic(image_id, owner_id, token, text, group_id):
     url = 'https://api.vk.com/method/wall.post'
     response = requests.post(url, data=payload)
     response.raise_for_status()
+    check_vk_response(response)
 
 
 def save_photo(photo, server, hash, token, group_id):
@@ -32,6 +38,7 @@ def save_photo(photo, server, hash, token, group_id):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     response = requests.post(url, data=payload)
     response.raise_for_status()
+    check_vk_response(response)
     return response.json()['response'][-1]
 
 
@@ -43,18 +50,19 @@ def upload_photo_to_address(name_folder, upload_url):
         }
         response = requests.post(url, files=files)
     response.raise_for_status()
+    check_vk_response(response)
     return response.json()
 
 
-def get_upload_url(client_id, token, group_id):
+def get_upload_url(token, group_id):
     payload = {
-        'client_id': client_id,
         'access_token': token,
         'v': '5.154',
         'group_id': group_id
     }
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     response = requests.get(url, params=payload)
+    check_vk_response(response)
     response.raise_for_status()
     return response.json()['response']['upload_url']
 
@@ -62,14 +70,17 @@ def get_upload_url(client_id, token, group_id):
 def get_comic(name_folder):
     last_comic_response = requests.get('https://xkcd.com/info.0.json')
     last_comic_response.raise_for_status()
+    check_vk_response(last_comic_response)
     last_comic_num = last_comic_response.json()['num']
     url = f'https://xkcd.com/{random.randint(0, last_comic_num)}/info.0.json'
     random_comic_response = requests.get(url)
     random_comic_response.raise_for_status()
+    check_vk_response(random_comic_response)
     comic= random_comic_response.json()
     Path(name_folder).mkdir(exist_ok=True)
     image_response = requests.get(comic['img'])
     image_response.raise_for_status()
+    check_vk_response(image_response)
     image = image_response.content
     with open(os.path.join(name_folder, f"image.png"), 'wb') as file:
         file.write(image)
@@ -78,16 +89,22 @@ def get_comic(name_folder):
 
 def main():
     load_dotenv()
-    client_id = os.environ["VK_CLIENT_ID"]
+    # client_id = os.environ["VK_CLIENT_ID"]
     vk_token = os.environ['VK_TOKEN']
     group_id = os.environ['VK_GROUP_ID']
     name_folder = 'files'
-    comic = get_comic(name_folder)
-    upload_url = get_upload_url(client_id, vk_token, group_id)
-    address = upload_photo_to_address(name_folder, upload_url)
-    album = save_photo(address['photo'], address['server'], address['hash'], vk_token, group_id)
-    publish_comic(album['id'], album['owner_id'], vk_token, comic['alt'], group_id)
-    shutil.rmtree(name_folder)
+    try:
+        comic = get_comic(name_folder)
+        upload_url = get_upload_url(vk_token, group_id)
+        address = upload_photo_to_address(name_folder, upload_url)
+        album = save_photo(address['photo'], address['server'], address['hash'], vk_token, group_id)
+        publish_comic(album['id'], album['owner_id'], vk_token, comic['alt'], group_id)
+    except requests.HTTPError:
+        print('HTTP error occurred')
+    except requests.ConnectionError:
+        print('Connection is interrupted')
+    finally:
+        shutil.rmtree(name_folder)
 
 
 if __name__ == "__main__":
